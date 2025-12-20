4fc3517f50e340b029d168ea60f3fa73788b0bdc
eviltouch-work <eviltouch008@gmail.com>
2025-12-20 16:11:54 +0530

    Create web-page.groovy

---
+++ web-page.groovy
@@ -0,0 +1,38 @@
+#for python test
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout from GitHub') {
+            steps {
+                // Replace with your GitHub repo URL
+                git branch: 'main', url: 'https://github.com/eviltouch-work/github-netlify.git'
+            }
+        }
+
+        stage('Prepare Webpage') {
+            steps {
+                // Copy all files into a folder for HTML Publisher
+                sh '''
+                mkdir -p website
+                cp index.html style.css website/
+                '''
+
+            }
+        }
+
+        stage('Publish Webpage') {
+            steps {
+                // Use HTML Publisher Plugin to show webpage in Jenkins
+                publishHTML(target: [
+                    allowMissing: false,
+                    alwaysLinkToLastBuild: true,
+                    keepAll: true,
+                    reportDir: 'website',
+                    reportFiles: 'index.html',
+                    reportName: 'My Webpage'
+                ])
+            }
+        }
+    }
+}
